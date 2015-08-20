c966ecf81ba62ea0d793e8a82ef78ecb0066ae2c
John Austin <johnau@microsoft.com>
2015-08-20 13:20:06 -0700

    Initial commit

---
+++ C#/DatePickerSampleWeb/Scripts/DatePickerSample.js
@@ -0,0 +1,62 @@
+﻿/*
+* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+*/
+
+// This function is run when the app is ready to start interacting with the host application
+// It ensures the DOM is ready before adding click handlers to buttons
+Office.initialize = function (reason) {
+    $(document).ready(function () {
+        // Set various options of the datepicker such as display
+        // multiple years in a downdown list, provide alternate
+        // date formatting, and prev and next month options.
+        var options = {
+            numberOfMonths: 1,
+            changeYear: true,
+            yearRange: "-2:+2",
+            altField: "#alternate",
+            altFormat: "DD, d MM, yy"
+        }
+        $("#datepicker").datepicker(options).click(function () {
+            $('#alternate').css('visibility', 'visible');
+        });
+
+        $('#setDateBtn').click(function () {
+            setData('#alternate');
+        });
+
+    });
+};
+
+// Writes data from textbox to the current selection in the document
+function setData(elementId) {
+    Office.context.document.setSelectedDataAsync($(elementId).val());
+}
+
+// *********************************************************
+//
+// Excel-Add-in-Create-Date-Picker, https://github.com/OfficeDev/Excel-Add-in-Create-Date-Picker
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// *********************************************************
